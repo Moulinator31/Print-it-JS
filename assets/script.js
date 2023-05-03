ee3d77189444b2d04638ea65c1aefcf51c7b1869
Elsa Tanguy <elsa.tang28@gmail.com>
2023-05-03 21:41:39 +0200

    initialize project

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,93 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+
+];
+
+const banner = document.getElementById('banner');
+const arrowleft = document.querySelector('.arrow_left');
+const arrowright = document.querySelector('.arrow_right');
+const imageElement = document.getElementById('slide');
+const nomElement = document.querySelector('#banner > p');
+const bulletElement = document.querySelector('.dots');
+
+const numberOfSlide = slides.length;
+
+let number = 0;
+
+for (let i = 0; i < slides.length; i++) {
+	// var html = '<div class="dot"></div>';
+	const dotSpan = document.createElement('span');
+	
+	if (i === 0) {
+		html = '<span class="dot dot_selected"></span>';
+	} else {
+		html = '<span class="dot"></span>'
+	}
+	
+	bulletElement.insertAdjacentHTML('beforeend', html);
+}
+
+function changeBulletSlide() {
+	const bullets = bulletElement.querySelectorAll('.dot');
+
+	for (let i = 0; i < bullets.length; i++) {
+		let currentBullet = bullets[i];
+
+		if (i === number) {
+			currentBullet.classList.add('dot_selected');
+		} else {
+			currentBullet.classList.remove('dot_selected');
+		}
+	}
+
+}
+
+/**const changeBulletSlide = () => {
+	var bullets = bulletElement.querySelectorAll('.dot');
+
+	for (var i = 0; i < bullets.length; i++) {
+		var currentBullet = bullets[i];
+		if (i == number) {
+			currentBullet.classList.add('dot_selected');
+		}
+		else {
+			currentBullet.classList.remove('dot_selected');
+		}
+	}
+
+}*/
+
+function showSlide() {
+    imageElement.src = `./assets/images/slideshow/${slides[number].image}`;
+    nomElement.innerHTML = slides[number].tagLine;
+
+    console.log(imageElement);
+    console.log(nomElement);
+}
+
+
+arrowleft.addEventListener("click", function () {
+    if (number == 0) {
+        number = numberOfSlide - 1;
+    } else {
+        number--;
+    }
+        console.log(arrowleft);
+		showSlide();
+		changeBulletSlide();
+});
+
+arrowright.addEventListener("click", function () {
+    if (number == numberOfSlide - 1) {
+        number = 0;
+    } else {
+        number++;
+    }
+    console.log(arrowright);
+	showSlide();
+	changeBulletSlide();
+});
+
+
+	
